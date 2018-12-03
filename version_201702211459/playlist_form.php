b0c1ee153610e819ca739366578ae0097b172202
Lunovox <Lunovox@users.noreply.github.com>
2018-12-03 01:50:16 -0200

    pequena correção de ortografia e charcodes.

--- version_201702211459/playlist_form.php
+++ version_201702211459/playlist_form.php
@@ -7,7 +7,7 @@
 	ini_set('display_errors', 'On'); 
 	//error_reporting(E_ALL | E_STRICT);
 	error_reporting(E_ERROR | E_STRICT | E_WARNING | E_PARSE);
-	//ini_set('default_charset','utf-8');
+	ini_set('default_charset','utf-8');
 	
 	date_default_timezone_set("America/Recife"); //-0300
 
@@ -30,10 +30,44 @@
 		if(getLogedType()=="owner" || getLogedType()=="moderator"){ ?>
 
 			<!DOCTYPE html>
-			<html>
+			<html lang="pt-br">
 				<head>
+					<meta charset="utf-8"/>
+					<meta name="viewport" content="width=300"/>
 					<title>ADICIONAR VÍDEO A LISTA</title>
 					<link rel="stylesheet" href="estilo_geral.css?update=<?=date('Y-m-d H:i:s');?>" media="screen"/>
+					<script>
+						/*
+						@licstart The following is the entire
+						license notice for the JavaScript code in this
+						page.
+
+						Copyright (C) 2015 Lunovox ( ͡° ͜ʖ ͡°)
+
+						The JavaScript code in this page is free
+						software: you can redistribute it and/or
+						modify it under the terms of the GNU General
+						Public License (GNU GPL) as published by the
+						Free Software Foundation, either version 3 of
+						the License, or (at your option) any later
+						version. The code is distributed WITHOUT ANY
+						WARRANTY; without even the implied warranty of
+						MERCHANTABILITY or FITNESS FOR A PARTICULAR
+						PURPOSE. See the GNU GPL for more details.
+
+						As additional permission under GNU GPL version
+						3 section 7, you may distribute non-source
+						(e.g., minimized or compacted) forms of that
+						code without the copy of the GNU GPL normally
+						required by section 4, provided you include
+						this license notice and a URL through which
+						recipients can access the Corresponding
+						Source.
+
+						@licend The above is the entire license notice
+						for the JavaScript code in this page.
+						*/
+					</script>
 					<script>
 						window.addEventListener("load", function(){
 							var search = document.getElementById("search");
@@ -139,7 +173,7 @@
 								</tr>
 								<tr>
 									<td>
-										<input id="search" list="sugestions" style="width:200px" placeholder="Digite o nome a lista..." datalist/>
+										<input id="search" list="sugestions" style="width:200px" placeholder="Digite o nome da lista..." datalist/>
 										<datalist id="sugestions"></datalist> 
 									</td>
 									<td>
